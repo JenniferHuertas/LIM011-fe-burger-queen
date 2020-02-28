8eb7577e4c75ee1b7debe5a929e6b5ec350e8795
jennlindley <jennifersu2059@gmail.com>
2020-02-27 19:21:16 -0500

    suma de precios

--- src/components/Desayuno.vue
+++ src/components/Desayuno.vue
@@ -1,27 +1,35 @@
 <template>
     <div>
-      <div class="d-flex align-content-start flex-wrap">
-        <div v-for="(el, index) in $store.state.bebidasCalientes"
-        :key="index.toString().concat('bc')">
-          <button v-if="el.Categoria == 'Desayuno'" @click="lista = showElements(lista, el)"
-          class="btn btn-success">{{ el.Nombre }} S/.{{ el.Precio }} </button>
-        </div>
+      <div class="container px-lg-5">
+        <div class="row row-cols-2">
+          <div class="col py-3 px-lg-5 border bg-light">
+            <div class="d-flex align-content-start flex-wrap">
+              <div v-for="(el, index) in $store.state.bebidasCalientes"
+              :key="index.toString().concat('bc')">
+                <button v-if="el.Categoria == 'Desayuno'" @click="lista = showElements(lista, el)"
+                class="btn btn-success">{{ el.Nombre }} S/.{{ el.Precio }} </button>
+              </div>
 
-        <div v-for="(el,index) in $store.state.bebidasFrias" :key="index.toString().concat('bf')">
-          <button v-if="el.Categoria == 'Desayuno'" @click="lista = showElements(lista, el)"
-          class="btn btn-success">{{ el.Nombre }} S/.{{ el.Precio }}</button>
-        </div>
+              <div v-for="(el,index) in $store.state.bebidasFrias"
+              :key="index.toString().concat('bf')">
+                <button v-if="el.Categoria == 'Desayuno'" @click="lista = showElements(lista, el)"
+                class="btn btn-success">{{ el.Nombre }} S/.{{ el.Precio }}</button>
+              </div>
 
-        <div v-for="(el, index) in $store.state.sandwich" :key="index.toString().concat('sw')">
-          <button v-if="el.Categoria == 'Desayuno'" @click="lista = showElements(lista, el)"
-          class="btn btn-success">{{ el.Nombre }} S/.{{ el.Precio }}</button>
+              <div v-for="(el, index) in $store.state.sandwich"
+              :key="index.toString().concat('sw')">
+                <button v-if="el.Categoria == 'Desayuno'" @click="lista = showElements(lista, el)"
+                class="btn btn-success">{{ el.Nombre }} S/.{{ el.Precio }}</button>
+              </div>
+            </div>
+
+            <ul v-for="(el) in lista" :key="el.id" class="list-group">
+              <li class="list-group-item">{{ el.Nombre }} S/.{{el.Precio}}</li>
+            </ul>
+            <span>{{ total }}</span>
+          </div>
         </div>
       </div>
-
-        <ul v-for="(el) in lista" :key="el.id" class="list-group">
-          <li class="list-group-item">{{ el.Nombre }} S/.{{el.Precio}}</li>
-        </ul>
-
 
     </div>
 </template>
@@ -39,5 +47,15 @@ export default {
   methods: {
     showElements,
   },
+  computed: {
+    total() {
+      return this.lista.reduce((a, b) => {
+        console.log(a, b);
+        return a + b.Precio;
+      }, 0);
+    },
+
+  },
+
 };
 </script>
